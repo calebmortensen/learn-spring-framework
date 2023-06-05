836d93fff9d6f85a0162c01bbeebc2ac81310c6b
calebmortensen <caleb.mortensen@live.com>
2023-06-05 13:01:20 -0700

    Primary and Qualifier annotations
    Spring Bean annotation examples

--- src/main/java/com/calebproject/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/calebproject/learnspringframework/HelloWorldConfiguration.java
@@ -1,10 +1,12 @@
 package com.calebproject.learnspringframework;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 
-record Person(String name, int age) {
+record Person(String name, int age, Address address) {
 	
 };
 
@@ -26,15 +28,41 @@ public class HelloWorldConfiguration {
 		return 44;
 	}
 	
-    @Bean
+    @Bean (name = "person1")
+    @Primary
     public Person person() {
-    	return new Person("John", 20);
+    	return new Person("John", 20, new Address("Main St", "Orlando"));
+    	
+    }
+    
+    @Bean (name = "person2")
+    public Person person2() {
+    	return new Person("John", 20, new Address("State St", "NY"));
+    	
+    }
+    
+    @Bean 
+    public Person person5Qualifier(String name, int age,  @Qualifier("address3qualifier") Address address) {
+    	return new Person(name, age, address);
     	
     }
     
     @Bean
+    public Person person2MethodCall() {
+    	return new Person(name(), age(), address());
+    	
+    }
+    
+    @Bean(name = "address2")
+    @Primary
     public Address address() {
     	return new Address("1234 Harbor Dr", "San Diego");
+    }
+    
+    @Bean(name = "address3")
+    @Qualifier("address3qualifier")
+    public Address address3() {
+    	return new Address("5678 Main", "Ontario");
     }
 	
 }
