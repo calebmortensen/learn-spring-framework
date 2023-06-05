e0f3bbb3da217ba113da5c3e4609813a86bf5543
calebmortensen <caleb.mortensen@live.com>
2023-06-05 09:34:38 -0700

    First Spring Bean
    Configuration example

--- src/main/java/com/calebproject/learnspringframework/App01GamingBasicJava.java
+++ src/main/java/com/calebproject/learnspringframework/App01GamingBasicJava.java
@@ -5,14 +5,17 @@ import com.calebproject.learnspringframework.game.MarioGame;
 import com.calebproject.learnspringframework.game.PacManGame;
 import com.calebproject.learnspringframework.game.SuperContraGame;
 
-public class AppGamingBasicJava {
+public class App01GamingBasicJava {
 
 	public static void main(String[] args) {
 		
 		//var game = new MarioGame();
 		//var game = new SuperContraGame();
-		var game = new PacManGame();
-		var gameRunner = new GameRunner(game);
+		
+		var game = new PacManGame(); //1. Object Creation
+		
+		var gameRunner = new GameRunner(game); //2. Object Creation + Wiring of Dependencies
+		
 		gameRunner.run();
 
 	}
