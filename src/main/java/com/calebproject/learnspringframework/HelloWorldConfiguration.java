c665908afdc0995a60a6f4a425f3424d968ed9f6
calebmortensen <caleb.mortensen@live.com>
2023-06-05 10:33:11 -0700

    Spring Foundational Example
    Using HelloWorld to use Annotations

--- src/main/java/com/calebproject/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/calebproject/learnspringframework/HelloWorldConfiguration.java
@@ -3,6 +3,16 @@ package com.calebproject.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
+record Person(String name, int age) {
+	
+};
+
+record Address(String streetAddress, String city) {
+	
+}
+
+
 @Configuration
 public class HelloWorldConfiguration {
 
@@ -10,4 +20,21 @@ public class HelloWorldConfiguration {
 	public String name() {
 		return "Caleb";
 	}
+	
+	@Bean
+	public int age() {
+		return 44;
+	}
+	
+    @Bean
+    public Person person() {
+    	return new Person("John", 20);
+    	
+    }
+    
+    @Bean
+    public Address address() {
+    	return new Address("1234 Harbor Dr", "San Diego");
+    }
+	
 }
